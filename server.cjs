240a0c41fa58e1cc162d56ed876e0a9e4ebbf190
JohnnyCulbreth <johnnyculbrethjr@yahoo.com>
2023-09-18 08:11:48 -0500

    latest progress

--- server.cjs
+++ server.cjs
@@ -12,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/static', express.static(path.join(__dirname)));
 
 app.get('/getCliaData', async (req, res) => {
   const stateCode = req.query.STATE_CD;
